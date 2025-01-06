50b85355846b0a11583402c3b08ac53682c3e36a
Ulises Farfan <ulisesfarfan296@gmail.com>
2025-01-06 14:53:17 -0300

    first commit

---
+++ src/main/java/com/accenturepracticaaa/accenture_practicaaa/AccenturePracticaaaApplication.java
@@ -0,0 +1,13 @@
+package com.accenturepracticaaa.accenture_practicaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccenturePracticaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccenturePracticaaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenturepracticaaa/accenture_practicaaa/AccenturePracticaaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenturepracticaaa.accenture_practicaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccenturePracticaaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
